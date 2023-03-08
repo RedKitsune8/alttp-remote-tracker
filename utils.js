444bf0b79ffe248c3dfb8207ffbedcb23b05cd90
RedKitsune8 <91728806+RedKitsune8@users.noreply.github.com>
2023-03-08 14:21:39 +0100

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,11 @@
+function toggle(item) {
+  var x = document.getElementById();
+  if (x === 'arco') {
+  if (document.getElementById('imgplus').src == 'Icon/bomb.png') {
+            document.getElementById('imgplus').src = 'Icon/bow.png';
+        }
+         else {
+           document.getElementById('imgplus').src = 'Icon/bomb.png';
+       }
+  }
+} 
